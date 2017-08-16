fd5ff087310a23bf17a5e186f7559588d5f1cf01
linwu <linwu@hengtiansoft.com>
2017-08-16 13:56:16 +0800

    0816 first test

---
+++ src/main/java/com/linwu/yuanqi/util/Test1.java
@@ -0,0 +1,12 @@
+package com.linwu.yuanqi.util;
+
+/**
+ * Created by linwu on 8/16/2017.
+ */
+public class Test1 {
+
+    public static void main(String[] args){
+        String b = "helloworld";
+    }
+
+}
